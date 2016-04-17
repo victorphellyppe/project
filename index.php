b174910755ee6cac592f00cfc0f2892377e57006
victorphellyppe <victorphellyppe@hotmail.com>
2016-04-17 17:38:31 -0300

    Added files via upload

--- index.php
+++ index.php
@@ -1,28 +1,17 @@
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Project Foundation</title>
-<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.css"/>
+<head> <title></title>
 </head>
 <body>
-	<div id="menu .dropdown-menu">
-	<?php require_once ("pag/menu.php");?>
-
-	</div>
-	<div>
-
-	<?php require_once ($_GET["../pag/menu"]); ?>
-	</div>
+<div>
 
 
 
+<?php require_once("menu.php"); ?>
+<div>
+	<?php require_once($_GET["arquivo"]); ?>
+</div>
+</div>
 
- <footer>
-    Victor Phellyppe Todos os direitos reservados - <?php
-    echo date('Y') ?>
- <footer>
 
 </body>
-
 </html>

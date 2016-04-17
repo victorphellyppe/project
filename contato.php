38dd2136f62f0bef0534d7bce254d19cb109d4eb
victorphellyppe <victorphellyppe@hotmail.com>
2016-04-17 17:39:30 -0300

    Added files via upload

--- contato.php
+++ contato.php
@@ -3,7 +3,7 @@
 <input name="nome" type="text" placeholder="Digite seu nome"/>
 <input name="email" type="text" placeholder="Digite seu email">
 <input name="assunto" type="text" placeholder="Digite o assunto"/>
-<textaarea rows="5" cols="30"></textaarea>
+<textaarea rows="5" cols="30"name="msg"></textaarea>
 <input type="submit" name="enviar" value="Enviaar"/>
 </form>
 </fieldset>
--- index.php
+++ index.php
@@ -1,17 +1,45 @@
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$assunto = $_POST['assunto'];
+$mensagem = $_POST['msg'];
+if(isset($_POST["enviar"])){
+	echo "Dados Enviados com sucesso! '$nome' '$email' $assunto $mensagem";
+}
+?>
 <html>
-<head> <title></title>
+<head>
+<meta charset="UTF-8">
+<title>Project Foundation</title>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.css"/>
+<script type="text/javascript">
+	function controle(contato) {
+	var test = document.contato.input.value;
+
+}
+
+
+</script>
 </head>
 <body>
-<div>
+	<div id="menu .dropdown-menu">
+	<?php require_once ("pag/menu.php");?>
+
+	</div>
+	<div>
+
+	<?php require_once ($_GET["../pag/menu"]); ?>
+	</div>
 
 
 
-<?php require_once("menu.php"); ?>
-<div>
-	<?php require_once($_GET["arquivo"]); ?>
-</div>
-</div>
 
+ <footer>
+    Victor Phellyppe Todos os direitos reservados - <?php
+    echo date('Y') ?>
+ <footer>
 
 </body>
+
 </html>

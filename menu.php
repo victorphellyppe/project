9a99dfd3761d515db78e04c85f8f69d50314b3da
victorphellyppe <victorphellyppe@hotmail.com>
2016-04-19 10:54:36 -0300

    Added files via upload

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <ul>
-		<li><a href="/pag/home.php">Home</a></li>
-        <li><a href="/pag/empresa.php">Empresa</a></li>
-        <li><a href="/pag/produtos.php">Produtos</a></li>
-        <li><a href="/pag/servicos.php">Serviços</a></li>
-        <li><a href="/pag/contato">Contato</a></li>
+		<li><a href="index.php">Home</a></li>
+        <li><a href="pag/empresa.php">Empresa</a></li>
+        <li><a href="pag/produtos.php">Produtos</a></li>
+        <li><a href="pag/servicos.php">Serviços</a></li>
+        <li><a href="pag/fale-com-nos.html">Contato</a></li>
 </ul>
